2dca5efbb74169c46601cc2026c5c89868e5ec10
Manuelsa96 <manuelsa96@gmail.com>
2020-03-25 11:50:37 +0100

    Added IEnrollment Repository

---
+++ ContosoUniversity.InFrastructure/IEnrollmentRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using ContosoUniversity.Infrastructure;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.InFrastructure
+{
+    public interface IEnrollmentRepository : IAsyncRepository<Enrollment>
+    {
+        Task<Enrollment> GetEnrollmentDetailsAsync(int id);
+    }
+}
